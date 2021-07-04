30e8c5c73e51015832ea06fa4622b7abbdcb6fe6
calinovidiu96 <calinovidiu96@gmail.com>
2021-07-04 17:31:45 +0300

    submit form to firebase

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -55,6 +55,11 @@ const cartReducer = (state, action) => {
 			totalAmount: updatedTotalAmount,
 		};
 	}
+
+	if (action.type === 'CLEAR') {
+		return defaultCartState;
+	}
+
 	return defaultCartState;
 };
 
@@ -72,11 +77,16 @@ const CartProvider = (props) => {
 		dispatCartAction({ type: 'REMOVE', id: id });
 	};
 
+	const clearCartHandler = () => {
+		dispatCartAction({ type: 'CLEAR' });
+	};
+
 	const cartContext = {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
+		clearCart: clearCartHandler,
 	};
 
 	return (
